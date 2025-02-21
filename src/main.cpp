6f17d0c7b14d090354ab644ab10a65456fa3cfde
articrus <165282270+articrus@users.noreply.github.com>
2025-02-21 11:12:35 -0500

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "ofMain.h"
+#include "ofApp.h"
+/*
+ * Written by: Gianni Coladonato
+ * ID: 2414537
+ * Assignment 2, 21-02-2025
+*/
+
+//========================================================================
+int main( ){
+
+	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
+	ofGLWindowSettings settings;
+	settings.setSize(1024, 768);
+	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
+
+	auto window = ofCreateWindow(settings);
+
+	ofRunApp(window, make_shared<ofApp>());
+	ofRunMainLoop();
+
+}
