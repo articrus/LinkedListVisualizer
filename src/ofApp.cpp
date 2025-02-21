5758054528e0aea1f0b150bc198e53a808da21bd
articrus <165282270+articrus@users.noreply.github.com>
2025-02-21 12:41:55 -0500

    -Got the linked list + bonus methods working, still need to test them before i work on the oscillating animations and panning

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -7,7 +7,11 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	//Generate a list of 5 random radii
+	for (int i = 0; i < 5; i++)
+	{
+		linkedlist.insertAtTail(5.0f * (i + 1));
+	}
 }
 
 //--------------------------------------------------------------
@@ -17,12 +21,33 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	ofSetColor(255, 255, 255);
+	ofDrawBitmapString(linkedlist.listCount(), 100, 100);
+	for (int i = 0; i < linkedlist.listCount(); i++) {
+		ofSetColor(0, 255, 0);
+		ofDrawCircle(ofGetWidth() * (i + 1) / 6, ofGetHeight() / 2, linkedlist.getRadiusAtPos(i));
+		ofSetColor(255, 255, 255);
+		ofDrawBitmapString(linkedlist.getRadiusAtPos(i), ofGetWidth() * (i + 1) / 6, ofGetHeight() / 2);
+	}
 }
 
-//--------------------------------------------------------------
+//-Processes the user's key inputs------------------------------
 void ofApp::keyPressed(int key){
-
+	key = std::tolower(key); //Make key lowercase (inputs are case-sensitive)
+	switch (key) {
+		//List keys
+		case 'q': break; //Insert at head
+		case 'w': break; //Insert at tail
+		case 'a': break; //Delete head node
+		case 's': break; //Delete at tail
+		case 'e': break; //Sort list in ascending order
+		//Animation keys
+		case 'z': break; //Increase oscillation
+		case 'x': break; //Decrease oscillation
+		//Camera keys
+		case OF_KEY_LEFT: break; //Pan left
+		case OF_KEY_RIGHT: break; //Pan right
+	}
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -26,9 +26,135 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 
+		//The Node struct for the LinkedList
 		struct Node {
-			int radius;
-			Node* next;
+			float radius; //The radius of the circle
+			Node* next; //The next node in the list
+		};
+
+		//The LinkedList class
+		class LinkedList {
+			Node* head; 
+
+		//Methods
+		public:
+
+			//Default constructor (makes head null)
+			LinkedList() : head(NULL) {}
+
+			//Constructor with a value (radius)
+			LinkedList(float radius) {
+				insertAtHead(radius);
+			}
+
+			//Insert Node at the head of the list
+			void insertAtHead(float newRadius) {
+				Node* newNode = new Node();
+				newNode->radius = newRadius;
+				newNode->next = head;
+				head = newNode;
+			}
+
+			//Insert node at the tail of the list
+			void insertAtTail(float newRadius){
+				Node* newNode = new Node();
+				newNode->radius = newRadius;
+				newNode->next = NULL;
+
+				//If head is null, make this node the head
+				if (!head) {
+					head = newNode;
+					return;
+				}
+
+				//Travel through list to find the last node
+				Node* tempNode = head;
+				while (tempNode->next) {
+					tempNode = tempNode->next;
+				}
+
+				//Make last node the new node
+				tempNode->next = newNode;
+			}
+
+			//Remove node at the head of the list
+			void removeAtHead() {
+				if (!head) {
+					return; //List empty
+				}
+
+				Node* tempNode = head;
+				head = head->next;
+				delete tempNode;
+			}
+
+			//Remove node at the tail of the list
+			void removeAtTail() {
+				if (!head) {
+					return; //List empty
+				}
+
+				//If there is only one node
+				if (!head->next) {
+					delete head;
+					head = NULL;
+					return;
+				}
+
+				//Travel through list to second to last node
+				Node* tempNode = head;
+				while (tempNode->next->next) {
+					tempNode = tempNode->next;
+				}
+
+				//Remove node at the tail
+				delete tempNode->next;
+				tempNode->next = NULL;
+			}
+
+			//Get the node at the given index
+			float getRadiusAtPos(int pos) {
+				if (pos < 0) {
+					return -1; //If index is less than 0
+				}
+
+				//If index is 0, return the head
+				if (pos == 0) {
+					return head->radius;
+				}
+
+				//Navigate through the list to find the node at the index
+				int currentIndex = 1;
+				Node* tempNode = head->next;
+				while (tempNode != nullptr) {
+					if (currentIndex == pos) {
+						return tempNode->radius;
+					}
+					tempNode = tempNode->next;
+					currentIndex++;
+				}
+
+				//If index is out of bounds
+				return -1;
+			}
+
+			//Return the number of nodes in the linked list
+			int listCount() {
+				//If no head, return 0
+				if (!head) {
+					return 0;
+				}
+
+				//Navigate through list and incremement the nodecount 
+				int nodeCount = 0;
+				Node* tempNode = head;
+				while (tempNode != nullptr) {
+					nodeCount++;
+					tempNode = tempNode->next;
+				}
+				return nodeCount;
+			}
 		};
 		
+		LinkedList linkedlist;
 };
